613a03646f1d5e21d6eb011b4734a5611668e13f
GGQ_sb1703 <gqguan@hotmail.com>
2020-03-10 17:20:27 +0800

    Modify files as listed

--- Ignition modeling/GetTemperatureEvolution.m
+++ Ignition modeling/GetTemperatureEvolution.m
@@ -4,7 +4,7 @@ eta = 5;
 options = odeset('RelTol', 1e-4, 'AbsTol', 1e-4);
 [time, temperature] = ode45(@HeatBalance, [0, 0.5e+2], TW_set, options);
 plot(time, log10(temperature))
-legend_str = sprintf('TW = %.1f K', TW_set);
+legend_str = sprintf('T_W = %.1f K, \\eta = %.1f', TW_set, eta);
 legend(legend_str, 'Location', 'best');
 xlabel('Time/[s]');
 ylabel('log10(Temperature/[K])');
--- a3_17.m
+++ a3_17.m
@@ -9,14 +9,14 @@ c1 = 0.62; clr = 0.2;
 % 湍流参数
 a = 0.08;
 % 原方型喷嘴尺寸
-b0 = 0.4;
+b0 = 0.4/2;
 %% 计算理论燃尽火焰长度，即式(3-97)
 % 无因次理论燃尽火焰长度X = x*a/R0
 X = 0.7*c1/clr*sqrt((1+c1)/(1+0.77*clr));
 % 原方型喷嘴截面积
 A0 = b0*b0;
 % 喷嘴特征长度
-L = [b0; sqrt(A0/pi/2); sqrt(A0/2); 0.2];
+L = [b0; sqrt(A0/pi/2); sqrt(A0/2); 0.2/2];
 str_nozzle = {'0.4x0.4 square', 'eq.Area 2 circular', ...
               'eq.Area 2w square', '0.2x0.4 square'};
 % 理论燃尽火焰长度x
