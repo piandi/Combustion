c803801fb10c56d94ab96487b129e1e7d270433d
GGQ@SB1703 <gqguan@hotmail.com>
2020-10-05 10:05:34 +0800

    添加“应用MATLAB进行课程公式推导”的示例文件

--- fig3_13.m
+++ fig3_13.m
@@ -1,16 +1,36 @@
-%% 不同环境加热作用下的轴线流速变化
-%   即获得P67，图3-13
-T10 = 5;
-X = 0.5:0.1:5;
-for i = 1:length(X)
-    x = X(i);
-    eq3_93 = @(y)(x - 0.7/y*sqrt((1+0.735*(T10-1)*y))); % 式(3-85)
-    Y(i) = fsolve(eq3_93, 1);
+%% 射流燃烧升温（T1/T0）对轴线上的无因次剩余温度、浓度的影响
+%   即获得P67中的图3-13
+T10 = [5 2 1];
+Xmesh = 0.5:0.1:5;
+Ymesh = zeros(size(Xmesh));
+yyaxis left
+for j = 1:length(T10)
+    for i = 1:length(Xmesh)
+        X = Xmesh(i);
+        eq3_93a = @(Y)(X - 0.7/Y*sqrt((1+0.735*(T10(j)-1)*Y))); % 式(3-93a)
+        Ymesh(i) = fsolve(eq3_93a, 1);
+    end
+    plot(Xmesh, Ymesh)
+    hold on;
 end
-
-plot(X, Y)
 xlim([0,5])
 ylim([0,1])
 xlabel('$ax/R_{0}$', 'Interpreter', 'latex')
-ylabel('$\Delta T_{zs}/T_1$', 'Interpreter', 'latex')
-hold on;
+ylabel('$\Delta T_{zs}/T_1$ or ($\Delta c_{zs}/c_1$)', 'Interpreter', 'latex')
+legend('T_1/T_0 = 5', 'T_1/T_0 = 2', 'T_1/T_0 = 1')
+% P67中的图3-12
+yyaxis right
+for j = 1:length(T10)
+    for i = 1:length(Xmesh)
+        X = Xmesh(i);
+        eq3_90a = @(Y)(X - 0.96/Y*sqrt((1+0.535*(T10(j)-1)*Y))); % 式(3-90a)
+        Ymesh(i) = fsolve(eq3_90a, 1);
+    end
+    plot(Xmesh, Ymesh)
+    hold on;
+end
+xlim([0,5])
+ylim([0,1])
+xlabel('$ax/R_{0}$', 'Interpreter', 'latex')
+ylabel('$w_{zs}/w_0$', 'Interpreter', 'latex')
+legend('T_1/T_0 = 5', 'T_1/T_0 = 2', 'T_1/T_0 = 1')
