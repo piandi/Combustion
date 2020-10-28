c47449638116d7c697c62c0f8a4bc0505928ba98
GGQ@SB1703 <gqguan@hotmail.com>
2020-10-28 17:18:24 +0800

    [debug] 修正理论燃尽火焰长度的R0计算

--- a3_17.m
+++ a3_17.m
@@ -8,21 +8,25 @@ clear;
 c1 = 0.62; clr = 0.2;
 % 湍流参数
 a = 0.08;
-% 原方型喷嘴尺寸
-b0 = 0.4/2;
+% 原方型喷嘴尺寸（边长）
+b0 = 0.4;
+% 原方型喷嘴截面积
+A0 = b0*b0;
 %% 计算理论燃尽火焰长度，即式(3-97)
 % 无因次理论燃尽火焰长度X = x*a/R0
 X = 0.7*c1/clr*sqrt((1+c1)/(1+0.77*clr));
-% 原方型喷嘴截面积
-A0 = b0*b0;
-% 喷嘴特征长度
-L = [b0; sqrt(A0/pi/2); sqrt(A0/2); 0.2/2];
+% 润湿周边
+WettedPerimeter = [4*b0; sqrt(8*pi*A0); sqrt(32*A0); 4*(0.2+0.4)];
+% 喷嘴特征长度（两倍水力半径）
+HydraulicRadius = A0./WettedPerimeter;
+R0 = HydraulicRadius*2;
 str_nozzle = {'0.4x0.4 square', 'eq.Area 2 circular', ...
               'eq.Area 2w square', '0.2x0.4 square'};
 % 理论燃尽火焰长度x
-x = X*L./a;
+x = X*R0./a;
 %% 输出
 prompt = 'Theoretical lengths of flame for varied nozzles with the same cross-section area are respectively listed as';
 fprintf('%s \n', prompt);
-output = table(L, x, 'RowNames', str_nozzle)
+output = table(R0, x, 'RowNames', str_nozzle);
+disp(output)
 fprintf('It indicates that the larger influent size of nozzle has the longer flame. \n')
